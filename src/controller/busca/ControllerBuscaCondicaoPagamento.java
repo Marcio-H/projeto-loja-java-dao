8967fae38cec255422cd2f0a359c44f44155253a
Julio Bonzanino <bonzaninojuliopereira@gmail.com>
2022-03-11 11:55:28 -0300

    controllers de busca funcionando com o enter

--- src/controller/busca/ControllerBuscaCondicaoPagamento.java
+++ src/controller/busca/ControllerBuscaCondicaoPagamento.java
@@ -1,8 +1,13 @@
 package controller.busca;
 
+import java.awt.event.ActionEvent;
+import static java.awt.event.KeyEvent.VK_ENTER;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.function.Consumer;
+import javax.swing.AbstractAction;
+import static javax.swing.JComponent.WHEN_FOCUSED;
+import static javax.swing.KeyStroke.getKeyStroke;
 import javax.swing.table.DefaultTableModel;
 import model.bo.CondicaoPagamento;
 import service.CondicaoPagamentoService;
@@ -27,17 +32,32 @@ public class ControllerBuscaCondicaoPagamento {
         this.tela.getTable().setSelectionMode(0);
         carregarEventListener();
         sairEventListener();
+        selectFirstRow();
+        carregarToEnterEventListener();
+    }
+
+    private void selectFirstRow() {
+        tela.getTable().requestFocus();
+        tela.getTable().setRowSelectionInterval(0,0);
     }
     
     private void carregarEventListener() {
         tela.getCarregar().addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                carregarEventAction(evt);
+                carregarEventAction();
             }
         });
     }
-
-    private void carregarEventAction(MouseEvent evt) {
+    private void carregarToEnterEventListener() {
+        tela.getTable().getInputMap(WHEN_FOCUSED).put(getKeyStroke(VK_ENTER, 0), "EVENTO");
+        tela.getTable().getActionMap().put("EVENTO", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                carregarEventAction();
+            }
+        });
+    }
+    private void carregarEventAction() {
         int index = tela.getTable().getSelectedRow();
         if (index >= 0) {
             Long id = (long) tela.getTable().getValueAt(index, 0);
